444b4b505ce7482be3c98fe32957d3f3d8eaf4f6
namroodinc <a_namrood@hotmail.com>
2018-07-14 23:56:02 +0100

    fixed up google rss getter

--- actions/articleToDb/viaGoogle.js
+++ actions/articleToDb/viaGoogle.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 import metadata from "html-metadata";
-import isoFetch from "isomorphic-fetch";
 import parseDomain from "parse-domain";
 import request from "superagent";
 import { parseString } from "xml2js";
@@ -10,8 +9,8 @@ import { parseString } from "xml2js";
 request
   .post(`http://localhost:${process.env.PORT}/api/search/publications`)
   .send({
-    'searchTerm': 'Daily Mirror', // TODO: Remove 'The Guardian' after testing
-    'newsApiIdOrNot': true
+    // 'searchTerm': 'The New Yorker',
+    // 'newsApiIdOrNot': true
   })
   .set('X-CORS-TOKEN', process.env.APIKEY)
   .set('Content-Type', 'application/json')
@@ -21,23 +20,23 @@ request
       return new Promise((resolve) => {
         const { domain, tld } = parseDomain(publication.url);
 
-        isoFetch(`${process.env.GOOGLE_NEWS_URL}/site:${domain}.${tld}`, {
-          method: 'GET'
-        })
-          .then(response => {
-            if (response.ok) return response.text();
-            throw new Error(`Fetch ${domain}.${tld} failed`);
-          })
-          .then(body => {
-            parseString(body, (err, result) => {
-              const stringifyResults = JSON.stringify(result.rss.channel[0].item, undefined, 3);
-              const res = JSON.parse(stringifyResults);
+        request
+          .get(`${process.env.GOOGLE_NEWS_URL}/site:${domain}.${tld}`)
+          .buffer()
+          .type('xml')
+          .end((err, res) => {
+            if (res.buffered) {
+              parseString(res.text, (err, result) => {
+                const stringifyResults = JSON.stringify(result.rss.channel[0].item, undefined, 3);
 
-              resolve({
-                publicationId: publication['_id'],
-                res
+                if (stringifyResults !== undefined) {
+                  resolve({
+                    publicationId: publication['_id'],
+                    res: JSON.parse(stringifyResults)
+                  });
+                }
               });
-            });
+            }
           });
       });
     });
@@ -47,30 +46,43 @@ request
 
         publications.map(publication => {
           const { publicationId, res } = publication;
+
           const articlesArray = res.map(article => {
             return new Promise((resolve) => {
-              const { pubDate, title, link } = article;
+              const { pubDate, link } = article;
 
-              metadata(link[0])
+              const url = link[0];
+
+              metadata(url)
                 .then((metadata) => {
-                  const { general, openGraph } = metadata;
+                  const { general, openGraph, twitter } = metadata;
 
-                  const { author } = general;
+                  const { author, keywords } = general;
                   const { tag } = openGraph;
 
+                  const authors = author === undefined ? [] : author.split(',');
+
+                  const datePublished = openGraph['published_time'] || pubDate[0];
+                  const description = twitter.description || openGraph.description || general.description;
+
+                  const title = openGraph.title || general.title;
+                  const trendsList = keywords || tag || [];
+                  const trends = Array.isArray(trendsList) ? trendsList : trendsList.split(',');
+
+                  const urlToImage = openGraph.image.url || '';
+
                   resolve({
-                    authors: author === undefined ? [] : author.split(','),
-                    datePublished: pubDate[0],
-                    // description,
+                    authors,
+                    datePublished,
+                    description,
                     publicationId,
                     // sentiment: sentimentAnalysis,
-                    title: title[0],
-                    trends: tag,
-                    url: link[0]
-                    // urlToImage
+                    title,
+                    trends,
+                    url,
+                    urlToImage
                   });
                 });
-
             });
           });
 
@@ -78,7 +90,6 @@ request
             .then((articles) => {
               console.log(articles);
             });
-
         });
 
       });
